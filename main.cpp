23b617572d54aafdbc5ba316369f453bc7b77422
noisegain <ponomarenko@niuitmo.ru>
2022-02-02 02:23:12 +0300

    Final

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include <vector>
 #include <omp.h>
+#include <string>
 #include <algorithm>
-#include "chrono"
+#include <cmath>
 
 using namespace std;
 
-inline unsigned char get_mn(const long *r, int need) {
+inline unsigned char get_mn(const long *r, long need) {
     long c = 0;
     for (int i = 0; i < 256; i++) {
         c += r[i];
@@ -17,7 +17,7 @@ inline unsigned char get_mn(const long *r, int need) {
     return 255;
 }
 
-inline unsigned char get_mx(const long *r, int need) {
+inline unsigned char get_mx(const long *r, long need) {
     long c = 0;
     for (int i = 255; i >= 0; i--) {
         c += r[i];
@@ -31,31 +31,51 @@ inline unsigned char get_mx(const long *r, int need) {
 inline unsigned char overflow(double x) {
     if (x > 255) return 255;
     if (x < 0) return 0;
-    return (int) x;
+    return (unsigned char) round(x);
 }
 
+int main(int argc, char *argv[]) {
 
+    if (argc < 5) {
+        cout << "invalid arguments" << endl;
+        return 0;
+    }
 
-int main() {
-    omp_set_num_threads(8);
-    const char *name = "/mnt/c/Users/ispon/CLionProjects/skakov/test/baboon_copy.pgm";
-    FILE* input = fopen(name, "rb");
+    int thr;
+    double threshold;
+
+    try {
+        thr = stoi(argv[1]);
+        threshold = stof(argv[4]);
+    } catch (const invalid_argument &e) {
+        cout << "Error in " << e.what() << endl << "It is not a number" << endl;
+        return 0;
+    }
+
+    thr = thr == 0 ? omp_get_max_threads() : thr;
+    omp_set_num_threads(thr);
+
+    FILE *input = fopen(argv[2], "rb");
+    if (!input) {
+        cout << "Wrong file name" << endl;
+        return 0;
+    }
     fseek(input, 0, SEEK_END);
     long size = ftell(input);
-    cout << size << endl;
     fseek(input, 0, SEEK_SET);
     auto *arr = new unsigned char[size];
     if (size != fread(arr, sizeof(unsigned char), size, input)) {
-        cout << "something went wrong while read, try again." << endl;
-        return 1337;
+        cout << "Something went wrong while read, try again." << endl;
+        return 0;
     }
     fclose(input);
-    auto start = chrono::high_resolution_clock::now();
+
     if (arr[0] != 'P' || (arr[1] != '5' && arr[1] != '6')) {
-        cout << "Broken file" << endl;
-        return 228;
+        cout << "It is not portable bitmap image file" << endl;
+        return 0;
     }
-    int w = 0, h = 0, pos = 2;
+    bool rgb = arr[1] == '6';
+    long w = 0, h = 0, pos = 2;
     while (isspace(arr[pos])) pos++;
     pos--;
     while (!isspace(arr[++pos])) {
@@ -68,75 +88,58 @@ int main() {
     }
     while (isspace(arr[pos++]));
     pos += 3;
-    while (isspace(arr[pos])) pos++;
-    cout << w << " " << h << endl;
 
-    long r[256]={0};
-    long g[256]={0};
-    long b[256]={0};
+    double start = omp_get_wtime();
 
-    auto start2 = chrono::high_resolution_clock::now();
+    long r[256] = {0};
+    long g[256] = {0};
+    long b[256] = {0};
 
-#pragma omp parallel sections shared(arr, pos, size, r, g, b) default(none)
-    {
-#pragma omp section
-        for (int i = pos; i < size; i += 3) {
-            r[arr[i]]++;
+    int step = rgb ? 3 : 1;
+
+    long block = size / thr;
+    while (rgb && block % 3) block++;
+#pragma omp parallel for shared(arr, block, pos, size, rgb, step, r, g, b, cout) default(none) schedule(dynamic)
+    for (long k = pos; k < size; k += block) {
+        long rc[256] = {0}, gc[256] = {0}, bc[256] = {0};
+        long end = min((long) size, k + block);
+        for (long i = k; i < end; i += step) {
+            rc[arr[i]]++;
+            if (rgb) {
+                gc[arr[i + 1]]++;
+                bc[arr[i + 2]]++;
+            }
         }
-#pragma omp section
-        for (int i = pos + 1; i < size; i += 3) {
-            g[arr[i]]++;
-        }
-#pragma omp section
-        for (int i = pos + 2; i < size; i += 3) {
-            b[arr[i]]++;
+#pragma omp critical
+        {
+            for (int i = 0; i < 256; i++) {
+                r[i] += rc[i];
+                g[i] += gc[i];
+                b[i] += bc[i];
+            }
         }
     }
 
+    long need = (long) (w * h * threshold);
+    unsigned char mn = rgb ? min({get_mn(r, need), get_mn(g, need), get_mn(b, need)}) : get_mn(r, need);
+    unsigned char mx = rgb ? max({get_mx(r, need), get_mx(g, need), get_mx(b, need)}) : get_mx(r, need);
 
-
-//    for (int i = pos; i < size; i += 3) {
-//        r[arr[i]]++;
-//        g[arr[i + 1]]++;
-//        b[arr[i + 2]]++;
-//    }
-
-    auto end1 = chrono::high_resolution_clock::now();
-    cout << "This: " << chrono::duration_cast<chrono::milliseconds>(end1 - start2).count() << "ms" << endl;
-
-    //unsigned char mn = *min_element(arr + pos, arr + size);
-    //unsigned char mx = *max_element(arr + pos, arr + size);
-
-    double threshold = 0.05;
-    int need = w * h * threshold;
-    unsigned char mn = min({ get_mn(r, need), get_mn(g, need), get_mn(b, need) });
-    unsigned char mx = max({ get_mx(r, need), get_mx(g, need), get_mx(b, need) });
-
-//    #pragma omp parallel for shared(arr, pos, block, size, mn, mx, cout) default(none) schedule(static, 100)
-//    for (int i = pos; i < size; i += block) {
-//        unsigned char curmn = 255;
-//        unsigned char curmx = 0;
-//        int end = min((int) size, i + block);
-//        for (int j = i; j < end; j++) {
-//            curmn = min(curmn, arr[j]);
-//            curmx = max(curmx, arr[j]);
-//        }
-//#pragma omp critical
-//        {
-//            mn = min(mn, curmn);
-//            mx = max(mx, curmx);
-//        }
-//    }
-    cout << (int) mn << " " << (int) mx << endl;
-    cout << size - pos << endl;
-#pragma omp parallel for shared(arr, pos, size, mn, mx) default(none) //schedule(static, 1000)
-    for (int i = pos; i < size; i++) {
-        arr[i] = overflow(255.0 * ((double) (arr[i] - mn) / (double) (mx - mn)));
+    cout << (int) mn << ' ' << (int) mx << endl;
+    unsigned char values[256];
+    for (int i = 0; i < 256; i++) {
+        values[i] = overflow(255.0 * ((double) (i - mn) / (double) (mx - mn)));
     }
-    auto stop = chrono::high_resolution_clock::now();
-    auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
-    cout << "Time: " << duration.count() << " ms" << endl;
-    FILE* output = fopen("/mnt/c/Users/ispon/CLionProjects/skakov/test/out.pgm", "wb");
+
+    if (mn != mx) {
+#pragma omp parallel for shared(arr, pos, size, values) default(none) schedule(dynamic, 65536)
+        for (long i = pos; i < size; i++) {
+            arr[i] = values[arr[i]];
+        }
+    }
+
+    cout << "Time (" << thr << " thread(s)): " << 1000 * (omp_get_wtime() - start) << " ms" << endl;
+    FILE *output = fopen(argv[3], "wb");
     fwrite(arr, sizeof(unsigned char), size, output);
     fclose(output);
+    delete[] arr;
 }
